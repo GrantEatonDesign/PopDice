b9ea9df96b3efb69007cdb9bb9905a5f03e7a246
Grant Eaton <77468235+GrantEatonDesign@users.noreply.github.com>
2021-03-01 15:29:40 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage = "images/dice" + randomNumber1 + "Custom.png";
+var image1 = document.querySelectorAll("img")[4];
+image1.setAttribute("src", randomDiceImage);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage2 = "images/dice" + randomNumber2 + "Custom.png";
+var image2 = document.querySelectorAll("img")[5];
+image2.setAttribute("src", randomDiceImage2);
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 1 Wins";
+} else if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins";
+} else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
